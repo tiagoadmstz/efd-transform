df61138249b245159927c066cdb8b5deab0bca15
Tiago Daniel Teixeira <tiago.teixeira@philips.com>
2025-01-03 16:29:06 -0300

    feat: adds block M

--- src/main/java/io/github/tiagoadmstz/blocks/m/DetalhamReceitasIsentasAliquotaZeroCofins.java
+++ src/main/java/io/github/tiagoadmstz/blocks/m/DetalhamReceitasIsentasAliquotaZeroCofins.java
@@ -12,6 +12,16 @@ import java.math.BigDecimal;
 public class DetalhamReceitasIsentasAliquotaZeroCofins extends AbstractEfdBlockPart {
 
     private final String reg = "M810";
+    /**
+     * Natureza da Receita, conforme relação constante nas Tabelas de Detalhamento da Natureza da Receita por Situação Tributária abaixo:
+     * - Tabela 4.3.10: Produtos Sujeitos à Incidência Monofásica da Contribuição Social – Alíquotas Diferenciadas (CST 04 - Revenda);
+     * - Tabela 4.3.11: Produtos Sujeitos à Incidência Monofásica da Contribuição Social – Alíquotas por Unidade de Medida de Produto (CST 04 - Revenda);
+     * - Tabela 4.3.12: Produtos Sujeitos à Substituição Tributária da Contribuição Social (CST 05 - Revenda);
+     * - Tabela 4.3.13: Produtos Sujeitos à Alíquota Zero da Contribuição Social (CST 06);
+     * - Tabela 4.3.14: Operações com Isenção da Contribuição Social (CST 07);
+     * - Tabela 4.3.15: Operações sem Incidência da Contribuição Social (CST 08);
+     * - Tabela 4.3.16: Operações com Suspensão da Contribuição Social (CST 09).
+     */
     private String natRec;
     private BigDecimal vlRec;
     private String codCta;
